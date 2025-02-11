3a652d28ca0c84860decf2fc081f1fea16915b84
keyno63 <kofujiw@gmail.com>
2025-02-11 17:47:50 +0900

    clickable

--- app/src/main/java/tokyo/keyno63/jackinpoint/MainActivity.kt
+++ app/src/main/java/tokyo/keyno63/jackinpoint/MainActivity.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import tokyo.keyno63.jackinpoint.theme.JackInPointTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,13 +29,23 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                            .clickable {
+                                val intent = Intent(this@MainActivity, ProductActivity::class.java).apply {
+                                    addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+                                }
+                                startActivity(intent)
+                            },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "画面をタップして遷移", fontSize = 20.sp)
+                    }
                 }
             }
         }
-        val intent = Intent(this, ProductActivity::class.java).apply {
-            addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
-        }
-        startActivity(intent)
     }
 }
 
